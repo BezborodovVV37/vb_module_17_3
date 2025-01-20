8a78275d3feeccc3ee05b4e1a4430371a1bcba44
BezborodovVV37 <blood371993@gmail.com>
2025-01-20 09:00:29 +0300

    Add files via upload

---
+++ alembic/version/aa24d72c5f80_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: aa24d72c5f80
+Revises: 
+Create Date: 2025-01-19 15:21:25.526503
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'aa24d72c5f80'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
