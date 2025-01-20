dfc5b6fdac1bf34f99747f974bee2d863e1084eb
BezborodovVV37 <blood371993@gmail.com>
2025-01-20 09:03:30 +0300

    Add files via upload

---
+++ alembic/version/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.routers import task, user
+
+app = FastAPI()
+
+app.include_router(task.router)
+app.include_router(user.router)
+
+
+@app.get('/')
+def welcome():
+    return {"message": "Welcome to Taskmanager"}
+# uvicorn app.main:app --reload
